613aff77af118a18bba13b188b89d9f26e81e2ce
Viqui Alaro <viquialaro241@gmail.com>
2021-08-10 23:45:37 -0400

    creación de folder y pagina de inicio

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+}
+
+
